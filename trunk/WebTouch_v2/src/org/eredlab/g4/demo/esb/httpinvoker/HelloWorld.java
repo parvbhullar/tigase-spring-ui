30331230ded107ce08d1965575d64384e73a6d00
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-06-02 08:58:02 +0000

    httpinvoke helloService

---
+++ trunk/WebTouch_v2/src/org/eredlab/g4/demo/esb/httpinvoker/HelloWorld.java
@@ -0,0 +1,37 @@
+package org.eredlab.g4.demo.esb.httpinvoker;
+
+import java.util.List;
+
+import org.eredlab.g4.ccl.datastructure.Dto;
+import org.eredlab.g4.ccl.datastructure.impl.BasePo;
+
+/**
+ * Httpinvoker接口
+ * 
+ * @author XiongChun
+ * @since 2010-10-13
+ * @see BasePo
+ */
+public interface HelloWorld {
+	/**
+	 * 需要对外发布的方法
+	 * @param text
+	 * @return
+	 */
+	public String sayHello(String text);
+	
+	/**
+	 * 查询一条结算明细测试数据
+	 * @param jsbh
+	 * @return XML字符串
+	 */
+	public Dto queryBalanceInfo(String jsbh);
+	
+	/**
+	 * 查询结算明细测试数据列表
+	 * @param rownum
+	 * @return List
+	 */
+	public List queryBalanceInfoLimitRownum(Integer rownum);
+	
+}
