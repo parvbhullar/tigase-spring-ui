6e0725b093533d862c50ff00e5d950838a643e89
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-06-14 08:10:13 +0000

    cdr queryForList

--- trunk/conference_v1/src/com/ivyinfo/cdr/services/CdrServices.java
+++ trunk/conference_v1/src/com/ivyinfo/cdr/services/CdrServices.java
@@ -1,9 +1,15 @@
 package com.ivyinfo.cdr.services;
 
+import java.util.List;
+
 import com.ivyinfo.cdr.bean.CdrBean;
 import com.ivyinfo.framework.service.base.IBaseService;
 
 public interface CdrServices extends IBaseService{
-	public void AddCdr(CdrBean cdr) throws Exception;
+	public int AddCdr(CdrBean cdr) throws Exception;
+	
+	public void updateCdr(CdrBean cdr) throws Exception;
+	
+	public List<CdrBean> queryCdr(CdrBean cdr) throws Exception;
 
 }
