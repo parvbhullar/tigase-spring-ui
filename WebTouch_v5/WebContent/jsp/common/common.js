cdebcc50fa736571b236e18e165ce19b8240da7f
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-07-01 02:52:25 +0000

    common.js

---
+++ WebTouch_v5/WebContent/jsp/common/common.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+$(document).ready( function() {
+	$.ajax({
+		  url: 'jsp/common/common.jsp',
+		  success: function(data) {
+		    $("#tabs-1").html(data);
+		  },
+		  complete:function(){
+		  }
+	});
+})
