17839625204c702ade6133bfc6365f9062bdf03c
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-08-02 03:49:34 +0000

    bind("click.jstree")

--- trunk/WebTouch_v5/WebContent/jsp/common/system.js
+++ trunk/WebTouch_v5/WebContent/jsp/common/system.js
@@ -2,12 +2,14 @@
  * 
  */
 $(document).ready( function() {
+	
 	$.ajax({
 		  url: 'jsp/common/common.jsp',
 		  success: function(data) {
 		    $("#tabs-1").html(data);
 		  },
 		  complete:function(){
+			  
 		  }
 	});
 	
