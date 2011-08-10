2c1cc5e93e242ac20fa8ce2553308f03cbdb1459
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-08-10 09:38:36 +0000

    date

--- WebTouch_v5/WebContent/jsp/date/date.js
+++ WebTouch_v5/WebContent/jsp/date/date.js
@@ -2,19 +2,6 @@
  * 
  */
 $(document).ready( function() {		
-	      /*添加按钮点击事件开始*/
-	      $('#add').click(function() { 
-	          $.blockUI({ 
-	              theme:     true, 
-	              title:    '添加用户', 
-	              message:  $("#addForm"),
-	              css: { 
-	            	  top:  ($(window).height() - 400) /2 + 'px', 
-	                  left: ($(window).width() - 400) /2 + 'px' 
-	              }
-	          }); 
-	      });
-	      
 	      $('#save').click(function() { 
 	    	  var validator=$('#userForm').validate({
 	    	    	rules: { 
@@ -42,25 +29,5 @@ $(document).ready( function() {
 		    		 }); 
 	    	  }
 	            return false; 
-	        });
-	      
-	      $('#cancel').click(function() { 
-	            $.unblockUI(); 
-	            return false; 
-	        });
-	      /*添加按钮点击事件结束*/
-	      
-	      $('#delete').click(function() { 
-	    	  var selarrrow=$("#listTable").getGridParam('selarrrow');
-	    	  $.ajax({
-	    		   type: "POST",
-	    		   url: "user/deleteUserItems.action",
-	    		   data: "selarrrow="+selarrrow,
-	    		   success: function(msg){
-	    			   $("#listTable").trigger("reloadGrid");
-	   	            	return false;
-	    		   }
-	    		 }); 
-	            return false;
 	        });
 })
