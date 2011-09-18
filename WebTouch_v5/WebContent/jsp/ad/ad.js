daa2b4b42b81ea8306bbb699401cc0c224f07f53
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-09-18 14:38:49 +0000

    地区选择

---
+++ WebTouch_v5/WebContent/jsp/ad/ad.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+$(document).ready( function() {		
+	      /*添加按钮点击事件开始*/
+	      $('#area').click(function() { 
+	          $.blockUI({ 
+	              theme:     true, 
+	              title:    '选择地区', 
+	              message:  $("#jobLayerId"),
+	              css: { 
+	            	  top:  ($(window).height() - 400) /2 + 'px', 
+	                  left: ($(window).width() - 400) /2 + 'px',
+	                  width: "100%"
+	                  
+	              }
+	          }); 
+	      });
+})
