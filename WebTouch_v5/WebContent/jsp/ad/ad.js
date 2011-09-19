bc3f56859d307ba476a58a9f3891c75b959b8be6
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2011-09-19 14:31:41 +0000

    地区选择树

--- WebTouch_v5/WebContent/jsp/ad/ad.js
+++ WebTouch_v5/WebContent/jsp/ad/ad.js
@@ -12,8 +12,26 @@ $(document).ready( function() {
 	            	  top:  ($(window).height() - 400) /2 + 'px', 
 	                  left: ($(window).width() - 400) /2 + 'px'
 	              }
-	          }); 
+	          });
+	          console.info($("#jobLayerId").css("width"));
+	          $("#jobLayerId").parent().parent().css("width",(($(window).width() - 400)+"px"));
+	          $("#jobLayerId").parent().parent().css("left",(($(window).width() - 700)/2+"px"));
+	          $("#jobLayerId").parent().parent().css("top",(($(window).height() - 600)/2+"px"));
+	          console.info($("#jobLayerId").parent().parent().css("width"));
+	          $("#jobLayerId tbody tbody td").hover(
+	        		  function () {
+	        			  $(this).removeClass("zz_51Lower");
+	        			  $(this).addClass("zz_51High"); 
+	        			  }, 
+	        			  function () { 
+	        			  $(this).removeClass("zz_51High");
+	        			  $(this).addClass("zz_51Lower");
+	        			  } 
+	        		  )
+	          
 	      });
+	      
+	      
 	      
 	      $('#cancel').click(function() { 
 	            $.unblockUI(); 
