4942eca6017759b5d5b9c3e6931d17681d87b4c7
qinkun1234 <qinkun1234@4acf1e67-aba8-a66a-3f9a-35fc814734f0>
2012-05-23 16:24:20 +0000

    raphael

---
+++ raphael/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<title>PHP Site Template</title>
+	<link rel="stylesheet" href="public/css/default.css" />
+	<link rel="stylesheet" href="public/js/ui-lightness/jquery-ui.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+	<script src="public/js/raphael-min.js"></script>
+	<script src="public/js/custom.js"></script>
+</head>
+<body>
+
+<!-- start:Wrap -->
+<div id="wrap">
+
+<?php include 'header.php'; ?>
+
+<!-- start:Content -->
+<div id="content">
+
+<div id="draw"></div>
+
+</div>
+<!-- end:Content -->
+
+<?php include 'footer.php'; ?>
+
+</div>
+<!-- end:Wrap -->
+</body>
+</html>
---
+++ raphael/public/js/custom.js
@@ -0,0 +1,6 @@
+$(function() {
+
+	var paper=Raphael('draw',400,200,0,0);
+	var c=paper.circle(20,20,20);
+	
+});
